a409aa7c659a4c8936f05d0ec92f593293868426
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-03-07 12:58:21 -0300

    Feat: It is now possible to add new contacts

--- src/model/User.js
+++ src/model/User.js
@@ -42,9 +42,39 @@ export class User extends Model{
 
     }
 
+    static getContactsRef(id){
+        return User.getRef().doc(id).collection('contacts');
+    }
+
     static findByEmail(email){
 
         return User.getRef().doc(email);
+
+    }
+
+    addContact(contact){
+        return User.getContactsRef(this.email).doc(btoa(contact.email)).set(contact.toJSON());
+    }
+
+    getContacts(){
+
+        return new Promise((s, f) => {
+
+            User.getContactsRef(this.email).onSnapshot(docs => {
+                let contacts = [];
+                console.log(docs);
+                docs.forEach(doc => {
+                    console.log(doc);
+                    let data = doc.data();
+                    data.id = doc.id;
+                    contacts.push(data);
+                });
+                this.trigger('contactschange', docs);
+
+                s(contacts);
+            });
+
+        });
 
     }
 
