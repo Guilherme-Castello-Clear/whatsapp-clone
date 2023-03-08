f331bd761245f7780e91d7efe09a643a085875b0
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-03-08 11:34:42 -0300

    Feat: Added auto scroll down

--- src/controller/WhatsappController.js
+++ src/controller/WhatsappController.js
@@ -165,15 +165,25 @@ export class WhatsappController{
             display: 'flex'
         });
 
+        this.el.panelMessagesContainer.innerHTML = ''
+
         Message.getRef(this._contactActive.chatId).orderBy('timeStamp').onSnapshot(docs=>{
-            this.el.panelMessagesContainer.innerHTML = ''
+            
+            
+
+            let scrollTop = this.el.panelMessagesContainer.scrollTop;
+            let scrollTopMax = 
+            (this.el.panelMessagesContainer.scrollHeight -
+            this.el.panelMessagesContainer.offsetHeight);
+            let autoScroll = (scrollTop >= scrollTopMax);
+
+
             docs.forEach(doc => {
 
                 let data = doc.data();
                 data.id = doc.id;
 
                 if(!this.el.panelMessagesContainer.querySelector('#_'+data.id)){
-
 
                     let message = new Message();
 
@@ -182,8 +192,19 @@ export class WhatsappController{
                     let view = message.getViewElement(me);
 
                     this.el.panelMessagesContainer.appendChild(view);
+
                 }
             })
+            if(autoScroll){
+
+                this.el.panelMessagesContainer.scrollTop =
+                (this.el.panelMessagesContainer.scrollHeight -
+                this.el.panelMessagesContainer.offsetHeight);
+
+            }
+            else{
+                this.el.panelMessagesContainer.scrollTop = scrollTop;
+            }
         })
     }
 
